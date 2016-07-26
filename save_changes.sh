06bb98dc36dd062d3354babcb2c4ee5e8a469255
Andrew Parrish <m.andrewparrish@gmail.com>
2016-07-25 23:47:57 -0400

    Added tmux support

--- save_changes.sh
+++ save_changes.sh
@@ -1,2 +1,3 @@
+cp ~/.tmux.conf .tmux.conf
 cp ~/.vimrc .vimrc
 cp ~/.zshrc_extras .zshrc_extras
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.dotfile_backups
 mv ~/.vimrc ~/.dotfile_backups/
 mv ~/.zshrc_extras ~/.dotfile_backups/
+mv ~/.tmux.conf ~/.dotfile_backups/
+cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
 cp .zshrc_extras ~/.zshrc_extras
